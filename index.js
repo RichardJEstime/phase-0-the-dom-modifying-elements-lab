bc6852aac5a5656fdabf942cfc451b4320e219ea
RichardEstime <richardjestime@yahoo.com>
2021-06-04 17:45:54 -0400

    commit tree 30bbd0643afebc48a8f80cde56196b080258870c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const elementMain= document.getElementById('main')
+elementMain.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Richard is the champion'
